030655d0b5e4740b9f5261d4a13b6ec8ff51eb18
moon9588 <moon9588@yahoo.com>
2022-05-03 02:51:23 -0400

    worked onscript.js and style.css

---
+++ script.js
@@ -0,0 +1,18 @@
+    var startBtn = document.getElementById("startBtn");
+    var time = 75;
+    var time_remaining = true;
+    var time_start= false;
+    var countdownTimer = document.getElementById("countdownTimer");
+    var homeContainer =  document.getElementById("homeContainer");
+    var quizContainer = document.getElementById("quizContainer");
+    var questionHeading = document.getElementById("questionHeading");
+    var answerChoiceA = document.getElementById("answerChoiceA");
+    var answerChoiceB = document.getElementById("answerChoiceB");
+    var answerChoiceC = document.getElementById("answerChoiceC");
+    var answerChoiceD = document.getElementById("answerChoiceD");
+    var correctAnswer = document.getElementById("correctAnswer");    
+    var high_scores= [];
+    var output="";
+    var score = 0;
+    let i = 0;
+
